1ecffb742609de7d38fa919b6c0730f58d8e9e84
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-06-28 17:12:15 -0700

    updated css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,46 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
-var wins = 0;
-var losses = 0;
-var numGuesses = 9;
-var guessChoices = [];
-
+let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+// Variables
+let wins = 0;
+let losses = 0;
+let numGuesses = 9;
+let guessChoices = [];
+//DOM events allow JavaScript to register different event handlers on elements in an HTML document.
 document.onkeyup = function(event) {
-
-  var userGuess = event.key;
-
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-  var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+  //keyboard activity 
+  let userGuess = event.key;
+//returns random element from string
+  let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// user guess options
+  let options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
   
-
+//user guess
  if (options.indexOf(userGuess) > -1) {
-
+//guesses correct win =+1
    if (userGuess === computerGuess) {
      wins++;
      numGuesses = 9;
      guessChoices = [];
    }
-
+//push() method adds new items to the end of an array, and returns the new length. decrement operator (--).
    if (userGuess != computerGuess) {
      numGuesses --;
      guessChoices.push(userGuess);
    }
-
+//  guesses number no change
    if (numGuesses === 0) {
-
    numGuesses = 9;
    losses ++;
    guessChoices = [];
-
-   
  }
 
- var html = 
+ let html = 
  "<h1> The Psychic Game </h1>" +
  "<p>Guess what letter I'm thinking of!</p>" +
  "<p>Wins: " + wins + "</p>" +
  "<p>Losses: " + losses + "</p>" +
  "<p>Guesses Left: " + numGuesses + "</p>" +
  "<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
-
+//print results 
  document.querySelector("#game").innerHTML = html;
 
  
